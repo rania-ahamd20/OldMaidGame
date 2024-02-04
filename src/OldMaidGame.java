6128d46f4ab8d7ada732bb4e2ac8b974d92078f6
Rania Ahmad <114476972+rania-ahamd20@users.noreply.github.com>
2024-02-04 22:24:31 +0300

    4-2-2024

--- src/OldMaidGame.java
+++ src/OldMaidGame.java
@@ -1,82 +1,64 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Semaphore;
-
-public class OldMaidGame implements CardGame {
+class OldMaidGame implements CardGame {
     private final List<Player> players;
     private final List<Card> deck;
-    private final Semaphore turnSemaphore;
+    private final Object gameLock = new Object();//My synchronization Object
 
     public OldMaidGame(int numPlayers) {
         players = new ArrayList<>();
         deck = initializeDeck();
-        turnSemaphore = new Semaphore(1);
         initializePlayers(numPlayers);
         distributeCards();
     }
-
+    Object getGameLock() {
+        return gameLock;
+    }
     private List<Card> initializeDeck() {
         List<Card> newDeck = new ArrayList<>();
-
         String[] cardTypes = {"Spades", "Clubs", "Diamonds", "Hearts"};
         String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         String[] colors = {"Black", "Black", "Red", "Red"};
 
         for (int i = 0; i < cardTypes.length; i++) {
-            for (int j = 0; j < values.length; j++) {
-                newDeck.add(new Card(values[j], cardTypes[i], colors[i]));
+            for (String value : values) {
+                newDeck.add(new Card(value, cardTypes[i], colors[i]));
             }
         }
         newDeck.add(new Card("Joker", "None", "Colorless"));
 
         Collections.shuffle(newDeck);
-
         return newDeck;
     }
 
     private void initializePlayers(int numPlayers) {
+        Semaphore turnSemaphore = new Semaphore(0);
         for (int i = 1; i <= numPlayers; i++) {
-            players.add(new Player("Player " + i, players, turnSemaphore));
+            players.add(new Player("Player " + i, players, turnSemaphore, getGameLock()));
         }
-    }
-    private void distributeCards() {
-        Iterator<Card> deckIterator = deck.iterator();
-        int playerIndex = 0;
-
-        while (deckIterator.hasNext()) {
-            Player currentPlayer = players.get(playerIndex);
-            Card currentCard = deckIterator.next();
-            currentPlayer.receiveCard(currentCard);
-            deckIterator.remove();
-            playerIndex = (playerIndex + 1) % players.size();
-        }
-
-        System.out.println("----------- Cards number after distribution ----------- ");
-        for (Player player : players) {
-            System.out.println(player.getName() + "'s hand size: " + player.getHand().size());
-        }
-
-        System.out.println("------- Hand of each Player after distribution ------- ");
-        for (Player player : players) {
-            System.out.println(player.getName() + "'s hand: " + player.getHand());
-        }
-        System.out.println("--------------------------------------------");
     }
 
     @Override
     public void startGame() {
-        for (Player player : players) {
-            player.start();
+        Thread[] playerThreads = new Thread[players.size()];
+
+        for (int i = 0; i < players.size(); i++) {
+            playerThreads[i] = new Thread(players.get(i), "PlayerThread-" + (i + 1));
+            playerThreads[i].start();
         }
 
         try {
-            for (Player player : players) {
-                player.join();
+            for (Thread thread : playerThreads) {
+                thread.join();
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        }
+
+        synchronized (gameLock) {
+            gameLock.notify();
         }
 
         displayResults();
@@ -104,11 +86,35 @@ public class OldMaidGame implements CardGame {
 
         System.out.print("Winners:");
         for (Player winner : winners) {
-            System.out.print(" "+winner.getName());
+            System.out.print(" " + winner.getName());
         }
         System.out.println();
     }
 
+    private void distributeCards() {
+        int playerIndex = 0;
+
+        for (Card card : deck) {
+            Player currentPlayer = players.get(playerIndex);
+            currentPlayer.receiveCard(card);
+            playerIndex = (playerIndex + 1) % players.size();
+        }
+
+        displayDistribution();
+    }
+
+    private void displayDistribution() {
+        System.out.println("----------- Cards number after distribution ----------- ");
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s hand size: " + player.getHand().size());
+        }
+
+        System.out.println("------- Hand of each Player after distribution ------- ");
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s hand: " + player.getHand());
+        }
+        System.out.println("--------------------------------------------");
+    }
 
     private boolean hasJoker(Player player) {
         for (Card card : player.getHand()) {
@@ -119,4 +125,3 @@ public class OldMaidGame implements CardGame {
         return false;
     }
 }
-
--- src/Player.java
+++ src/Player.java
@@ -2,68 +2,83 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Semaphore;
-
-public class Player extends Thread {
+import java.util.Iterator;
+class Player implements Runnable {
     private final String name;
     private final List<Card> hand;
     private final List<Player> players;
-    private int currentPlayerIndex;
     private final Semaphore turnSemaphore;
-
-    public Player(String name, List<Player> players, Semaphore turnSemaphore) {
-        super(name);
+    private final Object gameLock;
+    private static volatile int currentPlayerIndex = 0;
+    private final int playerIndex;
+    private static volatile boolean gameOngoing = true;
+    public Player(String name, List<Player> players, Semaphore turnSemaphore, Object gameLock) {
         this.name = name;
         this.players = players;
-        hand = new ArrayList<>();
-        currentPlayerIndex = players.indexOf(this);
+        this.hand = new ArrayList<>();
         this.turnSemaphore = turnSemaphore;
+
+        this.turnSemaphore.release(); // Release the semaphore initially
+
+        this.gameLock = gameLock;
+        this.playerIndex = players.indexOf(this);
     }
 
     @Override
     public void run() {
         while (!allHandsEmptyExceptOne()) {
             try {
-                turnSemaphore.acquire();
-                checkMatchingPair();
-                Player nextPlayer = getNextPlayerInTurnOrder();
-                Card card = takeRandomCard(nextPlayer);
-                if (card != null) {
-                    checkMatchingPair();
-                }
+                takeTurn();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-            } finally {
-                turnSemaphore.release();
             }
         }
     }
 
-    private Player getNextPlayerInTurnOrder() {
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
-        return players.get(currentPlayerIndex);
-    }
+    private void takeTurn() throws InterruptedException {
+        synchronized (gameLock) {
+            while (!isPlayerTurn()) {
+                gameLock.wait();
+            }
 
-    private void printAllHands() {
-        for (Player player : players) {
-            System.out.println(player.getName() + "'s hand: " + player.getHand());
+            Player nextPlayer = getNextPlayerInTurnOrder();
+            takeRandomCard(nextPlayer);
+            checkMatchingPair();
+
+            // This is where matching pairs are checked
+
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+            gameLock.notifyAll();
         }
-        System.out.println();
     }
 
-    private Card takeRandomCard(Player player) {
+
+    private boolean isPlayerTurn() {
+        return players.indexOf(this) == currentPlayerIndex;
+    }
+    private Player getNextPlayerInTurnOrder() {
+        return players.get((currentPlayerIndex + 1) % players.size());
+    }
+
+    private void takeRandomCard(Player player) {
         if (!player.equals(this) && !player.getHand().isEmpty()) {
             List<Card> playerHand = player.getHand();
             Collections.shuffle(playerHand);
-            Card randomCard = playerHand.remove(0);
-            hand.add(randomCard);
-            System.out.println(name + " took a card from " + player.getName() + " which is " + randomCard);
-            return randomCard;
+
+            // Use an Iterator to safely remove elements while iterating
+            Iterator<Card> iterator = playerHand.iterator();
+            if (iterator.hasNext()) {
+                Card randomCard = iterator.next();
+                iterator.remove();  // Safe removal using Iterator
+                hand.add(randomCard);
+                System.out.println(name + " took a card from " + player.getName() + " which is " + randomCard);
+            }
         }
-        return null;
     }
 
+
     private void checkMatchingPair() {
-        for (int i = 0; i < hand.size(); i++) {
+        for (int i = 0; i <= hand.size(); i++) {
             for (int j = i + 1; j < hand.size(); j++) {
                 Card card1 = hand.get(i);
                 Card card2 = hand.get(j);
@@ -73,20 +88,23 @@ public class Player extends Thread {
                     hand.remove(card2);
 
                     System.out.println(name + " discarded matching pair: " + card1 + " and " + card2);
-                    printAllHands();
+
                     i--;
                     break;
                 }
             }
         }
+        printAllHands();
     }
 
     public void receiveCard(Card card) {
         hand.add(card);
     }
-
-    public List<Card> getHand() {
-        return hand;
+    private void printAllHands() {
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s hand: " + player.getHand());
+        }
+        System.out.println();
     }
 
     private boolean allHandsEmptyExceptOne() {
@@ -98,7 +116,18 @@ public class Player extends Thread {
             }
         }
 
-        return nonEmptyHands == 1;
-    }
-}
+        // Set the gameOngoing flag to false if only one hand is non-empty
+        gameOngoing = nonEmptyHands > 1;
 
+        return !gameOngoing;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+}
